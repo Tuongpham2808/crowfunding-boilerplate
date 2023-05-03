6039751516d64b56b115f5472578930df0f92194
TuongGit <tuongphamdh@gmail.com>
2023-05-03 20:15:01 +0700

    sign up ui

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LayoutAuthentication from "layout/LayoutAuthentication";
+
+const SignUpPage = () => {
+  return (
+    <LayoutAuthentication heading="SignUp">
+      Trang SignUpPage Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laudantium beatae officiis doloremque quis velit neque nulla nam dolorem
+      ullam quam doloribus, cum sunt minima dicta sapiente corporis, aut porro
+      id!
+    </LayoutAuthentication>
+  );
+};
+
+export default SignUpPage;
